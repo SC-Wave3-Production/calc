d978ea2f6ab156741144ec170a5b1695bf899ada
Silenthunt <destroyer.rus@gmail.com>
2020-08-19 10:52:08 +0300

    сдвинул секцию script вниз

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttons = Array.from(document.querySelectorAll('.buttons-section'))
-let displayInput = document.querySelector('input')
-
+let displayInput = document.querySelector('.output-window')
+console.log('buttons=', buttons)
 buttons.forEach(function (entry) {
     entry.addEventListener("click", element => {
         if(element.target.matches('.button')){
