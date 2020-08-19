8c9a23f4ac8c3e1223c4265c874a5be8f8bb04c9
Silenthunt <destroyer.rus@gmail.com>
2020-08-19 16:21:46 +0300

    добавил немного логики калькулятора

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 const buttons = Array.from(document.querySelectorAll('.buttons-section'))
+let valueA = ''
+let valueB = ''
+let action = ''
 let displayInput = document.querySelector('.output-window')
-console.log('buttons=', buttons)
 buttons.forEach(function (entry) {
     entry.addEventListener("click", element => {
-        if(element.target.matches('.button')){
-            displayInput.value += element.target.innerText
-    }
+        if(element.target.matches('.button-number')){
+            if (!action) {
+                valueA += element.target.innerText
+                displayInput.value = valueA
+                return
+            }
+            valueB += element.target.innerText
+            displayInput.value = valueB
+            return
+        }
+        if(element.target.matches('.button-action')){
+            if (!valueA) {
+                valueA = '0'
+            }
+            action = element.target.innerText
+            return
+        }
+        if(element.target.matches('.button-service')){
+            if (element.target.innerText === '=') {
+                if (valueA && valueB && action) {
+                    let result = 0
+                    switch(action) {
+                        case '+': {
+                            result = Number(valueA) + Number(valueB)
+                            break
+                        }
+                        case '-': {
+                            result = Number(valueA) - Number(valueB)
+                            break
+                        }
+                        case '/': {
+                            result = Number(valueA) / Number(valueB)
+                            break
+                        }
+                        case '*': {
+                            result = Number(valueA) * Number(valueB)
+                            break
+                        }
+                    }
+                    displayInput.value = result
+                    valueA = result
+                    valueB = ''
+                }
+            }
+        }
     });
 });
